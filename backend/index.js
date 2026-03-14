920da07b1433953a0cd90dd7893a85204865128f
pasindu157 <pasinduh84@gmail.com>
2026-03-15 01:06:30 +0530

    updated my files

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ import cryptoRouter from './routes/cryptoRoutes.js'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(helmet())
 
 mongoose.connect(process.env.MONGO_URI)
